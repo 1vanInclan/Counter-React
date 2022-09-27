28d688bb8735c84f885988738cbd61d0f4437cdf
Ivan Inclan <ivan.inclan.327b@gmail.com>
2022-09-27 15:14:12 -0500

    Terminado

---
+++ src/HelloWorldApp.jsx
@@ -0,0 +1,14 @@
+// function App() {
+//     return (<h1>Hola Mundo</h1>);
+// }
+// export default App;
+
+import React from 'react'
+
+export const HelloWorldApp = () => {
+  return (
+    <h1>HelloWorldApp</h1>
+  );
+}
+
+// export const HelloWorldApp = () => <h1>Arrow function</h1>
